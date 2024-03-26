13d8d309001521deb6ad9f2365d2aa7871d4dc64
D1M1TR1Y <dimaharcenko513@gmail.com>
2024-03-26 18:55:59 +0600

    erpvk[perk[pv

--- karbek/karbek/urls.py
+++ karbek/karbek/urls.py
@@ -1,8 +1,8 @@
 """
-URL configuration for karbek project.
+URL configuration for NewsPortal project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,9 +14,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.contrib.auth.views import LoginView
+from django.urls import path, include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('main.urls')),
+    path('accounts/', include('django.contrib.auth.urls'))
+    # path('login/', LoginView.as_view(template_name='registration/login.html'), name='login')
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
